06a275ae241edb9c872d733f2c2721364a955bd7
yoav amar <yoav.6hutc@gmail.com>
2020-09-21 14:56:07 +0300

    create the server interface

---
+++ src/Server.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "ClientHandle.hpp"
+namespace serverSide{
+    class Server
+    {
+    private:
+        
+    public:
+        virtual void open(int16_t port,const client::ClientHandle& handeler) = 0;
+        virtual void stop() = 0;
+    };
+    
+}
