b52bf9471a4aac6a975f6143826391ae5ecbed8c
yoav amar <yoav.6hutc@gmail.com>
2020-09-21 15:58:23 +0300

    start implement the open

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,6 +12,21 @@
 #include "ClientHandle.hpp"
 #include "Server.hpp"
 
+#define THROW_SYSTEM_ERROR() \
+    throw std::system_error { errno, std::system_category() }
+
 void serverSide::SerialServer::open(int16_t port,const client::ClientHandle& handeler){
+    struct sockaddr_in address; 
+
+    
+    const auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        THROW_SYSTEM_ERROR();
+    }
+
+    //should add
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
 
 }
