6645466154efa924aefe67313c0548cb56a85561
yoav amar <yoav.6hutc@gmail.com>
2020-09-21 16:55:29 +0300

    rearrange the code

--- src/Server.cpp
+++ src/Server.cpp
@@ -15,8 +15,10 @@
 #define THROW_SYSTEM_ERROR() \
     throw std::system_error { errno, std::system_category() }
 
-void serverSide::SerialServer::open(uint16_t port,const client::ClientHandle& handeler){
-    struct sockaddr_in address; 
+
+
+void serverSide::AbstractServer::init(uint16_t port){
+        struct sockaddr_in address; 
 
     
     const auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -28,5 +30,10 @@ void serverSide::SerialServer::open(uint16_t port,const client::ClientHandle& ha
 
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
+}
+
+void serverSide::SerialServer::open(uint16_t port,const client::ClientHandle& handeler){
+    init(port);
+
 
 }
