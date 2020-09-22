148eb65b91431b0e01a12afc1b37a5494096d376
Itay Gradenwits <itay25433@gmail.com>
2020-09-22 14:26:32 +0300

    added 2 new methods to the hpp file "MazeState.hpp"

--- src/MazeState.hpp
+++ src/MazeState.hpp
@@ -7,6 +7,28 @@ private:
   int y;
 
 public:
+  /**
+   * @brief return the x position of this state.
+   *
+   * @return int
+   */
+  int getX();
+
+  /**
+   * @brief return the y position of this state.
+   *
+   * @return int
+   */
+  int getY();
+
+  /**
+   * @brief Get the All Possible Neighbors (as a vector of states) of this state
+   * by a given maze.
+   *
+   * @param maze is a maze to that the state represnt a boot on it.
+   * @return std::vector<mazeState> is a vector of all the neighbors boots of
+   * the boot that represent this state on the maze.
+   */
   std::vector<mazeState> getAllPossibleNeighbors(maze::Maze maze);
 };
 } // namespace state
