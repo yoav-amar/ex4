1889c600af972ec2f78a85224d8cd4ea52e06daf
Itay Gradenwits <itay25433@gmail.com>
2020-09-23 04:27:27 +0300

    implement the methods of the maze class.

---
+++ src/Maze.cpp
@@ -0,0 +1,31 @@
+#include "Maze.hpp"
+#include "MazeState.hpp"
+
+maze::Maze::Maze(matrix::Matrix mazeAsMatrix, state::MazeState startState, state::MazeState endState): 
+m_mazeAsMatrix(mazeAsMatrix), m_startState(startState.getX(), startState.getY()), m_endState(endState.getX(), endState.getY()) {}
+
+uint32_t maze::Maze::getHeight() {
+    return m_mazeAsMatrix.getHight();
+}
+
+uint32_t maze::Maze::getWidth() {
+    return m_mazeAsMatrix.getWidth();
+}
+
+matrix::Matrix maze::Maze::getMazeAsMatrix() {
+    auto m = std::make_unique<matrix::Matrix>(m_mazeAsMatrix);
+    return *m;
+}
+
+state::MazeState maze::Maze::getstartState() {
+    auto s = std::make_unique<state::MazeState>(m_startState.getX(), m_startState.getY());
+    return *s;
+}
+
+state::MazeState maze::Maze::getEndState() {
+    auto s = std::make_unique<state::MazeState>(m_endState.getX(), m_endState.getY());
+    return *s;   
+}
+
+
+
