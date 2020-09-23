15b09a8c5b9b39e3f23e9cbae584dffbb52ae0e7
Itay Gradenwits <itay25433@gmail.com>
2020-09-23 03:22:57 +0300

    fixed all the bugs.

--- src/MazeState.cpp
+++ src/MazeState.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <vector>
 #include "Matrix.hpp"
+#include <iostream>
 state::MazeState::MazeState(uint32_t x, uint32_t y) {
     m_x = x;
     m_y = y;
@@ -17,7 +18,7 @@ uint32_t state::MazeState::getY() {
 
 std::vector<state::MazeState> state::MazeState::getAllPossibleNeighbors(matrix::Matrix& matrix) {
    std::vector<state::MazeState> neighbors;
-    if (( m_x-1 ) >= 0 && matrix.getValue(m_x - 1, m_y) != -1) {
+    if (( m_x - 1 ) >= 0 && matrix.getValue(m_x - 1, m_y) != -1) {
        auto leftNeighbor = state::MazeState(m_x - 1, m_y);
        neighbors.push_back(leftNeighbor);
     }
@@ -26,11 +27,13 @@ std::vector<state::MazeState> state::MazeState::getAllPossibleNeighbors(matrix::
         neighbors.push_back(rightNeighbor);
     }
     if (( m_y - 1 ) >= 0 && matrix.getValue(m_x, m_y - 1) != -1) {
-       auto leftNeighbor = state::MazeState(m_x, m_y - 1);
+       auto lowerNeighbor = state::MazeState(m_x, m_y - 1);
+       neighbors.push_back(lowerNeighbor);
     }
     if(m_y + 1 < matrix.getHight() && matrix.getValue(m_x, m_y + 1) != -1) {
-        auto rightNeighbor = state::MazeState(m_x, m_y + 1);
-        neighbors.push_back(rightNeighbor);
+        std::cout<<"flag"<<std::endl;
+        auto upperNeighbor = state::MazeState(m_x, m_y + 1);
+        neighbors.push_back(upperNeighbor);
     }
-
+    return neighbors;
 }
--- src/MazeStateTester.cpp
+++ src/MazeStateTester.cpp
@@ -2,6 +2,12 @@
 #include "MazeState.hpp"
 #include <memory>
 #include <iostream>
+#include "Matrix.hpp"
+
+void printState(state::MazeState s) {
+    std::cout<<s.getX()<<","<<s.getY()<<std::endl;
+}
+
 int stateTester::MazeStateTester::check() {
     bool mistake = false;
     auto state = std::make_unique<state::MazeState>(1,2);
@@ -12,6 +18,18 @@ int stateTester::MazeStateTester::check() {
     if(2 != state->getY()) {
         std::cout<<"mistake at get Y, your Y ="<<state->getX()<<" but it is actualy 2"<<std::endl;
         mistake = true;
+    }
+    auto matrix = std::make_unique<matrix::Matrix>(3,3);
+    int count = 0;
+    for(int i = 0; i < 3; i ++) {
+        for(int j = 0; j < 3; j++) {
+            matrix->setValue(i,j,count);
+            ++count;
+        }
+    }
+    auto neighbors = std::vector<state::MazeState>(state->getAllPossibleNeighbors(*matrix));
+    for(int i = 0; i < neighbors.size(); i++) {
+        printState(neighbors.at(i));
     }
     if(!mistake) {
         std::cout<<"success"<<std::endl;
