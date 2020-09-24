683b243e377b37b97f95973f06b1ad0b4e5881ed
Itay Gradenwits <itay25433@gmail.com>
2020-09-24 22:59:24 +0300

    fix the state pair class according to the new design of the state class.

--- src/StatesPair.hpp
+++ src/StatesPair.hpp
@@ -10,11 +10,46 @@ namespace statesPair {
         state::MazeState m_prevState;
 
         public:
-        StatesPair(const state::MazeState cur, const state::MazeState prev);
+        /**
+         * @brief Construct a new States Pair object
+         * 
+         * @param cur is a state to define as the current state of the pair.
+         * @param prev is a state to define as the prev state of the pair.
+         */
+        StatesPair(const state::MazeState& cur, const state::MazeState& prev);
+
+        /**
+         * @brief Get the Prev object
+         * 
+         * @return state::MazeState is the previous state of the pair.
+         */
         state::MazeState getPrev() const;
+
+        /**
+         * @brief Get the Cur object
+         * 
+         * @return state::MazeState is the current state of the pair
+         */
         state::MazeState getCur() const;
+
+        /**
+         * @brief set the pair.
+         * 
+         * @param other is other pair to state our pair fields according to.
+         */
         void set(const statesPair::StatesPair& other);
-        std::string getStepFromPrevToCur();
+
+        /**
+         * @brief Get the Step From Prev To Cur object.
+         * 
+         * @return std::string is a string represent the step, can be ("Right, Left, Up, Down")
+         */
+        std::string getStepFromPrevToCur() const;
+
+        /**
+         * @brief Destroy the States Pair object
+         * 
+         */
         ~StatesPair();
     };
 }
