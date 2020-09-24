1244e8b9b71bed6732af09aa9c71f4779097ad9d
yoav amar <yoav.6hutc@gmail.com>
2020-09-24 10:51:21 +0300

    write the handle method

--- src/MyClientHandler.cpp
+++ src/MyClientHandler.cpp
@@ -5,25 +5,25 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <atomic>
 #include <unistd.h>
+#include <chrono>
 #include "MyClientHandler.hpp"
+
 void handle::ClientHandle::handleClient(std::uint16_t out,std::uint16_t in) const{
-    std::int16_t x = write(in, "7897", 4);
-    std::cout << x << std::endl;
-    std::cout <<"opp" << std::endl;
+    
+    std::atomic_bool stop(false);
     std::string buffer(1024, '\0');
     const std::string emptyString(1024, '\0');
-    std::cout <<"ouo" << std::endl;
-    while(true){
+    const std::chrono::seconds watingTime (5);
+    auto start = std::chrono::steady_clock::now();
+    auto end = start;
+    while(!stop){
         const auto numBytesRead = read(in, (void*)(buffer.data()), buffer.size() - 1);
         if (numBytesRead < 0) {
+            std::string tmp = "";
+            printMsg(in, 1, tmp);
             close(in);
         }
-        buffer[numBytesRead] = '\0';
-        if(buffer.compare(0, numBytesRead, "\0") != 0){
-            write(out, buffer.data(), numBytesRead);
-            buffer = emptyString;
-        }
     }
-    close(in);
 }
