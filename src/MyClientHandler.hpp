bc349ba616edd49b44dfc121c9d4cab5b405388f
yoav amar <yoav.6hutc@gmail.com>
2020-09-26 17:46:54 +0300

    added notes to my client handeler

--- src/MyClientHandler.hpp
+++ src/MyClientHandler.hpp
@@ -6,10 +6,19 @@
 #include "Problem.hpp"
 
 namespace handle{
+    /**
+     * @brief handle a client.
+     * connect with the client with a socket.
+     * 
+     */
     class ClientHandle{
-        private:
-
         public:
+    /**
+     * @brief handle the client.
+     * 
+     * @param out the socket for writing.
+     * @param in the socket for reading.
+     */
         void handleClient(std::uint16_t out,std::uint16_t in) const;
     };
 
