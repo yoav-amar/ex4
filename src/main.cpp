6fecad7d3da098ddddd8b454096681f36e9f78cd
yoav amar <yoav.6hutc@gmail.com>
2020-09-26 17:43:21 +0300

    throw exceptions in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Server.hpp"
+#include "CacheException.hpp"
 
 void initThreeArgs(serverSide::Server*& server, const std::string& kindOfServer){
     if(kindOfServer.compare("parallel") == 0){
@@ -9,11 +10,13 @@ void initThreeArgs(serverSide::Server*& server, const std::string& kindOfServer)
     }
     else
     {
-        //error
+        throw cacheExcption::CacheExcpetion(error::error_not_supported_this_kind_of_problem);
     }
 }
 
 int main(int argc, char** argv) {
+    try
+    {
     serverSide::Server* server;
     if(argc == 3){
         std::string kindOfServer = argv[2];
@@ -24,12 +27,13 @@ int main(int argc, char** argv) {
     }
     else
     {
-        //error
+        throw cacheExcption::CacheExcpetion(error::error_wrong_number_of_arguments);  
     }
-    try
-    {
     int port = std::stoi(argv[1]);
     server->open(port,handle::ClientHandle());
+    }
+    catch(const cacheExcption::CacheExcpetion& e){
+        e.printErrorMessage();
     }
     catch(const std::exception& e)
     {
